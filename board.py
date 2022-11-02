6c661f2b65321954761f5bdf9a9db4c5a75f59bb
ysh <yangshangmahua@gmail.com>
2022-11-02 19:52:01 -0400

    v1.0

---
+++ board.py
@@ -0,0 +1,15 @@
+class Board:
+    '''
+
+    '''
+    def __init__(board_size):
+        return 0
+        
+    def input():
+        
+
+class Location:
+    def __init__():
+        return 0
+        self.gold = False
+        self.wumpus = False 
