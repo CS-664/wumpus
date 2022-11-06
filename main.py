a3dfac6fccebc6f5466a43bb752df4fbcb59c5d4
UITraTheno <62661973+UITraTheno@users.noreply.github.com>
2022-11-06 15:12:53 -0500

    New Update
    Changed wrong input in txt file and finished testing board reading function

--- main.py
+++ main.py
@@ -2,13 +2,28 @@
 Import here
 '''
 
-def InitialBoard(txtName):
-    fileObject = open(txtName, "r")
-    data = fileObject.read()
-    print(data)
+def InitialBoard():
+    fileObject = open('BoardInfrom.txt', "r")
+    data = fileObject.read()          # Get the detail of testing board
+    BoardInform = data.split();
+    TBoard = [['','','','',''],
+              ['','','','',''],
+              ['','','','',''],
+              ['','','','',''],
+              ['','','','','']]                       # Initialize testing board
+    for i in range(len(BoardInform)-1):                                      # Set Special elements into initial board
+        iniboard = BoardInform[i].split(",")
+        if len(iniboard) == 3:
+            TBoard[int(iniboard[0])][int(iniboard[1])] = iniboard[2]
+        else:
+            TBoard[int(iniboard[0])][int(iniboard[1])] = iniboard[2] + ',' + iniboard[3]
+    return TBoard             # return testing board
+
 
 def main():
-    InitialBoard('BoardInfrom.txt')
+    a = InitialBoard()
+    print(a)
+
 
 if __name__ == "__main__":
     main()
