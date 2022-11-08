a36dbe4575561f3360b10e34c3e95fb5e51c20bd
ysh <yangshangmahua@gmail.com>
2022-11-08 17:26:28 -0500

    Case 1 finished

--- agent.py
+++ agent.py
@@ -1,3 +1,8 @@
+__all__ = [
+    'Actions',
+    'KBAgent',
+    'KB',
+]
 class Actions(enum.Enum):
    left = 1
    right = 2
@@ -7,12 +12,9 @@ class Actions(enum.Enum):
    shoot = 6
 
 class KBAgent:
-    def __init__(self,loc, board_size):
-        self.kb = KB(board_size)# knowledge base
+    def __init__(self, x, y, board_size):
+        self.kb = KB(x, y, board_size)# knowledge base
         self.t = 0 #time counter
-        self.loc = loc 
-        self.score = 0 #Gold +1000, Death -1000, Step -1, Shoot -10
-        self.arrow = True
 
     def perceive(self):
         curloc = self.loc 
@@ -36,20 +38,145 @@ class KBAgent:
         elif z == 'b':
             self.kb.breeze[x][y] = True
         elif z == 'g':
-            self.kb.breeze[x][y] = True 
+            self.kb.gold[x][y] = True 
 
     def ask(self):
         return 0
     
 class KB:
-    def __init__(self, board_size):
-        self.wumpus = [[False for x in range(board_size)] for y in range(board_size)]
-        self.pit = [[False for x in range(board_size)] for y in range(board_size)]
+    def __init__(self, x, y, board_size):
+        # -1 for unknow; 0 for False; 0-1 for probability; 1 for True
+        self.wumpus = [[-1.0 for x in range(board_size)] for y in range(board_size)]
+        self.pit = [[-1.0 for x in range(board_size)] for y in range(board_size)]
         self.breeze = [[False for x in range(board_size)] for y in range(board_size)]
         self.stench = [[False for x in range(board_size)] for y in range(board_size)]
         self.gold = [[False for x in range(board_size)] for y in range(board_size)]
-        self.loc = [[False for x in range(board_size)] for y in range(board_size)]
-        self.rule = set()
+        self.safe = [[False for x in range(board_size)] for y in range(board_size)]
+        self.visited = [[False for x in range(board_size)] for y in range(board_size)]
+        self.arrow = True
+        self.x = x
+        self.y = y
     
     def update(self):
-        
+        x, y = self.x, self.y
+        if self.visited[x][y]:
+            return 
+        self.visited[x][y] = True
+        self.safe[x][y] = True
+        #Case 1: No Stench or Breeze
+        if not self.stench[x][y] and not self.breeze[x][y]:
+
+            if self.pit[x][y] == -1.0:
+                self.pit[x][y] = 0.0
+            elif self.pit[x][y] > 0.0:
+                self.pit[x][y] = 0.0
+                for s in breeze_neighbor(x,y):
+                    cal_pit(s[0],s[1]) 
+
+            if self.wumpus[x][y] == -1.0:
+                self.wumpus[x][y] = 0.0
+            elif self.wumpus[x][y] > 0.0:
+                self.wumpus[x][y] = 0.0
+                for s in stench_neighbor(x,y):
+                    cal_wumpus(s[0],s[1])
+
+            for loc in neighbor(x,y):
+                nx, ny = loc[0], loc[1]
+                if self.pit[nx][ny] == -1.0:
+                    self.pit[nx][ny] = 0.0
+                elif self.pit[nx][ny] > 0.0:
+                    self.pit[nx][ny] = 0.0
+                    for s in breeze_neighbor(nx,ny):
+                        cal_pit(s[0],s[1]) 
+
+                if self.wumpus[nx][ny] == -1.0:
+                    self.wumpus[nx][ny] = 0.0
+                elif self.wumpus[nx][ny] > 0.0:
+                    self.wumpus[nx][ny] = 0.0
+                    for s in stench_neighbor(nx,ny):
+                        cal_wumpus(s[0],s[1])
+
+                self.safe[loc[0]][loc[1]] = True 
+        #Case 2: Stench and Breeze
+        elif self.stench[x][y] and self.breeze[x][y]:
+            candidate = []
+            for loc in neighbor(x,y):
+                nx, ny = loc[0], loc[1]
+                if not self.visited[nx][ny] and not self.safe[nx][ny]:
+                    candidate.append([nx,ny])
+                #clear pit probability
+                if self.pit[nx][ny] != 0.0:
+                    self.pit[nx][ny] = 0.0
+                    for s in breeze_neighbor(nx,ny):
+                        cal_pit(s[0],s[1])
+            for i in range(len(candidate)):
+                self.wumpus[candidate[i][0]][candidate[i][1]] = 1/len(candidate)
+        #Case 3: Breeze
+        elif self.breeze[x][y]:
+            candidate = []
+            for loc in neighbor(x,y):
+                nx, ny = loc[0], loc[1]
+                if self.pit[nx][ny] != 0.0:
+                    candidate.append([nx,ny])
+                #clear wumpus probability
+                if self.wumpus[nx][ny] != 0.0:
+                    self.wumpus[nx][ny] = 0.0
+                    for s in stench_neighbor(nx,ny):
+                        cal_wumpus(s[0],s[1])
+            for i in range(len(candidate)):
+                self.pit[candidate[i][0],candidate[i][1]] = 1/len(candidate)
+        #Case 4: Stench
+        elif self.stench[x][y]:
+            candidate = []
+            
+    def cal_wumpus(x,y): 
+        #recalculate the probability of wumpus around the stench
+        potential_neighbor = neighbor(x,y)
+        legal_wumpus = []
+        for loc in potential_neighbor:
+            nx, ny = loc[0], loc[1]
+            if 0.0 < self.wumpus[nx][ny] < 1.0 or self.wumpus[nx][ny] == -1.0:
+                legal_wumpus.append([nx,ny])
+        for i in range(len(legal_wumpus)):
+            self.wumpus[legal_wumpus[i][0]][legal_wumpus[i][1]] = 1/len(legal_wumpus)
+
+    def cal_pit(x,y):
+        #recalculate the probability of pit around the breeze
+        potential_neighbor = neighbor(x,y)
+        legal_pit = []
+        for loc in potential_neighbor:
+            nx, ny = loc[0], loc[1]
+            if 0.0 < self.pit[nx][ny] < 1.0 or self.wumpus[nx][ny] == -1.0:
+                legal_pit.append([nx,ny])
+        for i in range(len(legal_pit)):
+            self.pit[legal_pit[i][0]][legal_pit[i][1]] = 1/len(legal_pit)
+
+    def stench_neighbor(x,y):
+        res = []
+        dir = [-1, 0, 1, 0, -1]
+        for i in range(4):
+            nx = x + dir[i]
+            ny = y + dir[i+1]
+            if 0 <= nx <= len(self.loc)-1 and 0 <= ny <= len(self.loc)-1 and self.stench[nx][ny]:
+                res.append([nx, ny])
+        return res
+
+    def breeze_neighbor(x,y):
+        res = []
+        dir = [-1, 0, 1, 0, -1]
+        for i in range(4):
+            nx = x + dir[i]
+            ny = y + dir[i+1]
+            if 0 <= nx <= len(self.loc)-1 and 0 <= ny <= len(self.loc)-1 and self.breeze[nx][ny]:
+                res.append([nx, ny])
+        return res
+
+    def neighbor(x,y):
+        res = []
+        dir = [-1, 0, 1, 0, -1]
+        for i in range(4):
+            nx = x + dir[i]
+            ny = y + dir[i+1]
+            if 0 <= nx <= len(self.loc)-1 and 0 <= ny <= len(self.loc)-1:
+                res.append([nx, ny])
+        return res
