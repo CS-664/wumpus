7cdcf4e6f5029a61d15ac147a3866420dce321fc
Shanghua Yang <ysh99226@gmail.com>
2022-11-10 16:14:22 -0500

    findPath waited to be finished

--- agent.py
+++ agent.py
@@ -38,7 +38,8 @@ class KBAgent:
     def act(self):
         self.perceive()
         actions = self.ask()
-        self.tell()#Tell actions
+        for action in actions:
+            self.tell(0,0,action)
         return actions
     
     def tell(self, x, y, z):
@@ -60,7 +61,7 @@ class KBAgent:
         elif z == Actions.right:
             if self.kb.dir == Directions.North:
                 self.kb.dir = Directions.East
-            elif self.kb.dir == Dirctions.East:
+            elif self.kb.dir == Directions.East:
                 self.kb.dir = Directions.South
             elif self.kb.dir == Directions.South:
                 self.kb.dir = Directions.West
@@ -69,7 +70,7 @@ class KBAgent:
         elif z == Actions.forward:
             if self.kb.dir == Directions.North:
                 self.kb.x = self.kb.x - 1
-            elif self.kb.dir == Dirctions.East:
+            elif self.kb.dir == Directions.East:
                 self.kb.y = self.kb.y + 1
             elif self.kb.dir == Directions.South:
                 self.kb.x = self.kb.x + 1
@@ -79,23 +80,31 @@ class KBAgent:
             self.kb.shoot()
 
     def ask(self):
-        res = []
         k = self.kb 
         x ,y = self.kb.x, self.kb.y 
         #Gold
         if self.gold[x][y] == True:
             return [Actions.grab]
-        
+        locations = self.potential_loc()
+        if len(locations) != 0:
+            for loc in locations:
+                return self.findPath(x,y,loc[0],loc[1])
+        else:
+            #Find and kill wumpus
+            return []
 
-    def findPath(startx, starty, endx, endy):
+
+    def findPath(self,startx, starty, endx, endy):
         return [] 
 
     #need to implement possibility of killing wumpus to get potential loc
-    def potential_loc():
+    def potential_loc(self):
         res = []
         for i in range(len(self.kb.safe)):
             for j in range(len(self.kb.safe[0])):
                 if self.kb.safe[i][j] and not self.kb.visited[i][j]:
+                    res.append([i,j])
+        return res
 
     
 class KB:
@@ -119,6 +128,7 @@ class KB:
             return 
         self.visited[x][y] = True
         self.safe[x][y] = True
+        self.checksafe(len(self.pit),len(self.pit[0]))
         #Case 1: No Stench or Breeze
         #Do I need check current pit/wumpus possibility? 72 and 79
         if not self.stench[x][y] and not self.breeze[x][y]:
@@ -127,31 +137,31 @@ class KB:
                 self.pit[x][y] = 0.0
             elif self.pit[x][y] > 0.0:
                 self.pit[x][y] = 0.0
-                for s in breeze_neighbor(x,y):
-                    cal_pit(s[0],s[1]) 
+                for s in self.breeze_neighbor(x,y):
+                    self.cal_pit(s[0],s[1]) 
 
             if self.wumpus[x][y] == -1.0:
                 self.wumpus[x][y] = 0.0
             elif self.wumpus[x][y] > 0.0:
                 self.wumpus[x][y] = 0.0
-                for s in stench_neighbor(x,y):
-                    cal_wumpus(s[0],s[1])
+                for s in self.stench_neighbor(x,y):
+                    self.cal_wumpus(s[0],s[1])
 
-            for loc in neighbor(x,y):
+            for loc in self.neighbor(x,y):
                 nx, ny = loc[0], loc[1]
                 if self.pit[nx][ny] == -1.0:
                     self.pit[nx][ny] = 0.0
                 elif self.pit[nx][ny] > 0.0:
                     self.pit[nx][ny] = 0.0
-                    for s in breeze_neighbor(nx,ny):
-                        cal_pit(s[0],s[1]) 
+                    for s in self.breeze_neighbor(nx,ny):
+                        self.cal_pit(s[0],s[1]) 
 
                 if self.wumpus[nx][ny] == -1.0:
                     self.wumpus[nx][ny] = 0.0
                 elif self.wumpus[nx][ny] > 0.0:
                     self.wumpus[nx][ny] = 0.0
-                    for s in stench_neighbor(nx,ny):
-                        cal_wumpus(s[0],s[1])
+                    for s in self.stench_neighbor(nx,ny):
+                        self.cal_wumpus(s[0],s[1])
 
                 self.safe[loc[0]][loc[1]] = True 
         #Case 2: Stench and Breeze
@@ -159,7 +169,7 @@ class KB:
             #Probably have to check pit prob and wumpus prob of current loc
             stench_candidate = []
             breeze_candidate = []
-            for loc in neighbor(x,y):
+            for loc in self.neighbor(x,y):
                 nx, ny = loc[0], loc[1]
                 if self.pit[nx][ny] == -1.0 or 0.0 < self.pit[nx][ny] < 1.0:
                     breeze_candidate.append([nx,ny])
@@ -173,30 +183,30 @@ class KB:
         elif self.breeze[x][y]:
             #Probably have to check pit prob and wumpus prob of current loc
             candidate = []
-            for loc in neighbor(x,y):
+            for loc in self.neighbor(x,y):
                 nx, ny = loc[0], loc[1]
                 if self.pit[nx][ny] == -1.0 or 0.0 < self.pit[nx][ny] < 1.0:
                     candidate.append([nx,ny])
                 #clear wumpus probability
                 if 0.0 < self.wumpus[nx][ny] < 1.0:
                     self.wumpus[nx][ny] = 0.0
-                    for s in stench_neighbor(nx,ny):
-                        cal_wumpus(s[0],s[1])
+                    for s in self.stench_neighbor(nx,ny):
+                        self.cal_wumpus(s[0],s[1])
             for i in range(len(candidate)):
                 self.pit[candidate[i][0],candidate[i][1]] = 1/len(candidate)
         #Case 4: Stench
         elif self.stench[x][y]:
             #Probably have to check pit prob and wumpus prob of current loc
             candidate = []
-            for loc in neighbor(x,y):
+            for loc in self.neighbor(x,y):
                 nx, ny = loc[0], loc[1]
                 if self.wumpus[nx][ny] == -1.0 or 0.0 < self.wumpus[nx][ny] < 1.0:
                     candidate.append([nx,ny])
                 #clear pit probability
                 if 0.0 < self.pit[nx][ny] < 1.0:
                     self.pit[nx][ny] = 0.0
-                    for s in breeze_neighbor(nx,ny):
-                        cal_pit(s[0],s[1])
+                    for s in self.breeze_neighbor(nx,ny):
+                        self.cal_pit(s[0],s[1])
             for i in range(len(candidate)):
                 self.wumpus[candidate[i][0],candidate[i][1]] = 1/len(candidate)
         
@@ -213,9 +223,9 @@ class KB:
             self.wumpus[self.x][y-1] = 0.0
         self.arrow = False  
             
-    def cal_wumpus(x,y): 
+    def cal_wumpus(self,x,y): 
         #recalculate the probability of wumpus around the stench
-        potential_neighbor = neighbor(x,y)
+        potential_neighbor = self.neighbor(x,y)
         legal_wumpus = []
         for loc in potential_neighbor:
             nx, ny = loc[0], loc[1]
@@ -224,9 +234,9 @@ class KB:
         for i in range(len(legal_wumpus)):
             self.wumpus[legal_wumpus[i][0]][legal_wumpus[i][1]] = 1/len(legal_wumpus)
 
-    def cal_pit(x,y):
+    def cal_pit(self,x,y):
         #recalculate the probability of pit around the breeze
-        potential_neighbor = neighbor(x,y)
+        potential_neighbor = self.neighbor(x,y)
         legal_pit = []
         for loc in potential_neighbor:
             nx, ny = loc[0], loc[1]
@@ -235,7 +245,7 @@ class KB:
         for i in range(len(legal_pit)):
             self.pit[legal_pit[i][0]][legal_pit[i][1]] = 1/len(legal_pit)
 
-    def stench_neighbor(x,y):
+    def stench_neighbor(self,x,y):
         res = []
         dir = [-1, 0, 1, 0, -1]
         for i in range(4):
@@ -245,7 +255,7 @@ class KB:
                 res.append([nx, ny])
         return res
 
-    def breeze_neighbor(x,y):
+    def breeze_neighbor(self,x,y):
         res = []
         dir = [-1, 0, 1, 0, -1]
         for i in range(4):
@@ -255,7 +265,7 @@ class KB:
                 res.append([nx, ny])
         return res
 
-    def neighbor(x,y):
+    def neighbor(self,x,y):
         res = []
         dir = [-1, 0, 1, 0, -1]
         for i in range(4):
@@ -264,3 +274,10 @@ class KB:
             if 0 <= nx <= len(self.loc)-1 and 0 <= ny <= len(self.loc)-1:
                 res.append([nx, ny])
         return res
+    
+    #Can be modified
+    def checksafe(self,x,y):
+        for i in range(x):
+            for j in range(y):
+                if self.pit[i][j] == 0.0 and self.wumpus[i][j] == 0.0:
+                    self.safe[i][j] = True
