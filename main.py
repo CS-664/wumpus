16f02dd62426d5a6b9d4722d543d0e813e18df47
UITraTheno <62661973+UITraTheno@users.noreply.github.com>
2022-11-11 14:31:01 -0500

    initial board update
    Set the initial game board

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-'''
-Import here
-'''
+from board import *
+from agent import *
+import os
+
+def informChecker(w):
+    if w == 'G' or 'g':
+        return 'gold'
+    elif w == 'W' or 'w':
+        return 'wumpus'
+    elif w == 'B' or 'b':
+        return 'breezy'
+    elif w == 'S' or 's':
+        return 'stench'
+    elif w == 'P' or 'p':
+        return 'pit'
 
 def InitialBoard():
     fileObject = open('BoardInfrom.txt', "r")
     data = fileObject.read()          # Get the detail of testing board
     BoardInform = data.split();
-    TBoard = [['','','','',''],
-              ['','','','',''],
-              ['','','','',''],
-              ['','','','',''],
-              ['','','','','']]                       # Initialize testing board
-    for i in range(len(BoardInform)-1):                                      # Set Special elements into initial board
+    TBoard = Board(4,4)                 # initial game board
+    for i in range(len(BoardInform)-1):     # Update information following given map
         iniboard = BoardInform[i].split(",")
-        if len(iniboard) == 3:
-            TBoard[int(iniboard[0])][int(iniboard[1])] = iniboard[2]
-        else:
-            TBoard[int(iniboard[0])][int(iniboard[1])] = iniboard[2] + ',' + iniboard[3]
-    return TBoard             # return testing board
+        for n in range(2,len(iniboard)):
+            TBoard.input(int(iniboard[0]),int(iniboard[1]),informChecker(iniboard[n]))
+    return TBoard
+
 
 
 def main():
-    a = InitialBoard()
-    print(a)
+    gameBoard = InitialBoard()      # initializate game information
+    gameAgent = KBAgent(0,0,25)
+    gameScore = 100
+
+    gameAgent.act()
+
 
 
 if __name__ == "__main__":
